1bdfb554847eca07a197f3ce6b595ca9323110de
Hector Rivas Gandara <keymon@gmail.com>
2010-06-11 14:06:54 +0200

    -

--- misc/greasemonkey/autocontinue_websense.user.js
+++ misc/greasemonkey/autocontinue_websense.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name           	AutoContinue WebSense Block Page
-// @namespace      keymon
-// @include 	http://*:15871/*
+// @name AutoContinue WebSense Block Page
+// @namespace keymon
+// @include http://*:15871/*
 // ==/UserScript==
 //javascript:window.parent.frames[1].document.ContinueForm.submit();
 window.parent.frames[1].document.ContinueForm.submit();
