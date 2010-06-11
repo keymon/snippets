682af5d82b9c42552acbace8247b2063d791adae
Hector Rivas Gandara <keymon@gmail.com>
2010-06-11 13:58:49 +0200

    Added Greasemonkey userscript: AutoContinue WebSense Block Page

---
+++ misc/greasemonkey/autocontinue_websense.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           	AutoContinue WebSense Block Page
+// @namespace      keymon
+// @include 	http://*:15871/*
+// ==/UserScript==
+//javascript:window.parent.frames[1].document.ContinueForm.submit();
+window.parent.frames[1].document.ContinueForm.submit();
+//javascript:content.document.ContinueForm.submit();
+content.document.ContinueForm.submit();
+
